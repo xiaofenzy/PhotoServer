81ad17aa5e07f29c43f9af8db594b192f6a9e932
= <=>
2013-08-29 13:49:50 +0800

    与redis通信使用pipeline,缓存文件访问流等优化

--- src/op/StorePhoto.java
+++ src/op/StorePhoto.java
@@ -7,8 +7,12 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.net.InetAddress;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.List;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Pipeline;
 
 import common.RedisFactory;
 
@@ -19,10 +23,11 @@ public class StorePhoto {
 	private String confPath = "conf.txt";			//配置文件
 	private String remoteRedisHost;							//远程redis服务器地址
 	private int remoteRedisPort;							//端口号
-	private int localRedisPort;								//本地redis服务端口
 	private int dirnum;								//子目录层数	
 	private long blocksize;							//文件块的大小，单位是B
 	private String curBlock = null;					//当前可写的块
+	private RandomAccessFile raf = null;					//写当前的块的随机访问流
+	private Hashtable<String,RandomAccessFile> readRafHash;			//读文件时的随机访问流，用哈希来缓存
 	private Jedis jedis,jedis1;
 	
 	public StorePhoto()
@@ -42,8 +47,6 @@ public class StorePhoto {
 						remoteRedisHost = ss[1];
 					if(ss[0].equals("remoteRedisPort"))
 						remoteRedisPort = Integer.parseInt(ss[1]);
-					if(ss[0].equals("localRedisPort"))
-						localRedisPort = Integer.parseInt(ss[1]);
 					if(ss[0].equals("serverport"))
 						serverport = Integer.parseInt(ss[1]);
 					if(ss[0].equals("dirnum"))
@@ -53,12 +56,12 @@ public class StorePhoto {
 					
 				}
 			}
-//			jedis = new Jedis(host,port);
-			jedis = RedisFactory.getNewRemoteInstance(remoteRedisHost, remoteRedisPort);
-			jedis1 = RedisFactory.getNewLocalInstance(localRedisPort);
+			jedis = RedisFactory.getNewInstance(remoteRedisHost, remoteRedisPort);
+			jedis1 = RedisFactory.getNewInstance1(remoteRedisHost,remoteRedisPort);
 			br.close();
 			
 			localHostName = InetAddress.getLocalHost().getHostName();
+			readRafHash = new Hashtable<String,RandomAccessFile>();
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -98,84 +101,89 @@ public class StorePhoto {
 		File dir = new File(path);			
 		if (!dir.exists())
 			dir.mkdirs();
-		//找到当前可写的文件块,如果当前不够大,或不存在,则新创建一个,命名block＿id,id递增
+		//找到当前可写的文件块,如果当前不够大,或不存在,则新创建一个,命名block＿id,id递增,redis中只存储id
 		//用curBlock缓存当前可写的块，减少查询jedis的次数
 		File newf;
-		if(curBlock == null)
+		StringBuffer rVal = new StringBuffer();
+		try 
 		{
-			if(jedis.get(set) != null)
+			if (curBlock == null) 
 			{
 				curBlock = jedis.get(set);
-				newf = new File(path+curBlock);
-			}
-			else
+				if (curBlock != null)
+				{
+					newf = new File(path + "b" + curBlock);
+				} 
+				else 
+				{
+					curBlock = "1";
+					newf = new File(path + "b" + curBlock);
+					jedis1.set(set, curBlock);
+				}
+				raf = new RandomAccessFile(newf, "rw");
+			} 
+			else 
 			{
-				curBlock = "block_1";
-				newf = new File(path+curBlock);
-				jedis1.set(set, curBlock);
+				newf = new File(path + "b" + curBlock);
+				if (raf == null)
+					raf = new RandomAccessFile(newf, "rw");
+				if (newf.length() + content.length > blocksize) 
+				{
+					curBlock = String.valueOf((Integer.parseInt(curBlock) + 1));
+					newf = new File(path + "b" + curBlock);
+					// jedis1.set(set, curBlock);
+					raf = new RandomAccessFile(newf, "rw");
+					jedis1.incr(set);
+				}
 			}
-			
-		}
-		else
-		{
-			newf = new File(path+curBlock);
-			if(newf.length()+content.length > blocksize)
-			{
-				curBlock ="block_"+ (Integer.parseInt(curBlock.split("_")[1]) + 1);
-				newf = new File(path+curBlock);
-				jedis1.set(set, curBlock);
-			}
-		}
-		
-		location = path+newf.getName();				//文件块的完整路径名，包括文件名
-			StringBuffer rVal = new StringBuffer();
-			try {
-				
-				RandomAccessFile raf = new RandomAccessFile(newf, "rw");
-				offset = raf.length();
-				raf.seek(offset);
-				raf.write(content);
-				raf.close();
-				//构造返回值
-				rVal.append("1#");			//type
-				rVal.append(set);
-				rVal.append("#");
-				rVal.append(localHostName);			//node
-				rVal.append("#");
-				rVal.append(serverport);			//port
-				rVal.append("#");
-				rVal.append(location);
-				rVal.append("#");
-				rVal.append(offset);
-				rVal.append("#");
-				rVal.append(length);
-//				rVal.append("#");
-//				rVal.append(1);				//ref
-//				rVal.append("#");
-//				rVal.append(1);				//exist
+			// 文件块的路径名，包括文件名,不包括destRoot指定的第一级目录,解析时需要加上
+			location = path.substring(destRoot.length()) + newf.getName();
 
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			String returnVal = rVal.toString();
-			//确保多个进程生成的字符串只有一个被记录下来并且被完整的记录下
-			if(jedis.setnx(md5, returnVal) == 1)
-			{
-				jedis.incr("r."+md5);
-				return returnVal;
-			}
-			else
-			{
-				jedis.incr("r."+md5);				//增加引用计数
-				return jedis.get(md5);
-			}
-		
-		
+			offset = raf.length();
+			raf.seek(offset);
+			raf.write(content);
+			// raf.close();
+			// 构造返回值
+			rVal.append("1#"); // type
+			rVal.append(set);
+			rVal.append("#");
+			rVal.append(localHostName); // node
+			rVal.append("#");
+			rVal.append(serverport); // port
+			rVal.append("#");
+			rVal.append(location);
+			rVal.append("#");
+			rVal.append(offset);
+			rVal.append("#");
+			rVal.append(length);
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		String returnVal = rVal.toString();
+		// 确保多个进程生成的字符串只有一个被记录下来并且被完整的记录下
+		Pipeline pipeline = jedis.pipelined();
+		pipeline.incr("r." + md5);
+		pipeline.setnx(md5, returnVal);
+		List<Object> l = pipeline.syncAndReturnAll();
+//		System.out.println(l.get(1).getClass());
+		if((Long)l.get(1) == 1)
+			return returnVal;
+		else
+			return "#";
+//		if (jedis.setnx(md5, returnVal) == 1) {
+//			jedis.incr("r." + md5);
+//			return returnVal;
+//		} else {
+//			jedis.incr("r." + md5); // 增加引用计数
+//			return jedis.get(md5);
+//		}
+
 	}
 	/**
 	 * 存储多个图片，通过反复调用storePhoto实现
@@ -210,12 +218,12 @@ public class StorePhoto {
 	 */
 	public byte[] getPhoto(String md5)
 	{
-		if(jedis.get(md5) == null)
+		String info = jedis.get(md5);
+		if(info == null)
 		{
 			System.out.println("图片不存在:"+md5);
 			return null;
 		}
-		String info = jedis.get(md5);
 		return searchPhoto(info);
 	}
 	/**
@@ -225,13 +233,23 @@ public class StorePhoto {
 	 */
 	public byte[] searchPhoto(String info)
 	{
-		String[] infos = info.split("#");			
+		String[] infos = info.split("#");		
+		String path = infos[4];
+		RandomAccessFile readr = null;
 		byte[] content = new byte[Integer.parseInt(infos[6])];
 		try {
-			RandomAccessFile raf = new RandomAccessFile(infos[4], "r");
-			raf.seek(Long.parseLong(infos[5]));
-			raf.read(content);
-			raf.close();
+			//用哈希缓存打开的文件随机访问流
+			if(readRafHash.containsKey(path))
+			{
+				readr = readRafHash.get(path);
+			}
+			else
+			{
+				readr = new RandomAccessFile(destRoot+path, "r");
+				readRafHash.put(path, readr);
+			}
+			readr.seek(Long.parseLong(infos[5]));
+			readr.read(content);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -249,16 +267,26 @@ public class StorePhoto {
 	}
 	
 	@Override
-	protected void finalize()		//在该类被回收时关闭jedis连接
+	protected void finalize()		//在该类被回收时关闭jedis连接,关闭文件访问流
 	{
 		try {
 			super.finalize();
-		} catch (Throwable e) {
+			if(raf != null)
+				raf.close();
+			Enumeration<RandomAccessFile> er = readRafHash.elements();
+			while(er.hasMoreElements())
+				er.nextElement().close();
+		} 
+		catch(IOException e){
+			e.printStackTrace();
+		}
+		catch (Throwable e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		jedis.quit();
 		jedis1.quit();
+		
 	}
 	
 }
