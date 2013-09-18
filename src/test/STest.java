926c2bbb57350113264792ec22182ae2c6a9b85b
= <=>
2013-09-18 15:25:00 +0800

    启动server时,通过命令行参数传入节点名

--- src/test/STest.java
+++ src/test/STest.java
@@ -1,6 +1,7 @@
 package test;
 
-import java.io.File;
+
+import common.LocalHostName;
 
 import zy.PhotoServer;
 
@@ -8,15 +9,10 @@ public class STest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		if(args.length>0)
+			LocalHostName.setName(args[0]);
 		PhotoServer ps = new PhotoServer();
 		ps.startUp();
-//		Jedis j = new Jedis("127.0.0.1",6379);
-//		j.select(0);
-//		System.out.println(j.get("004.gif"));
-//		File dir = new File("photo/");
-//		for(File f : dir.listFiles())
-//			if(f.isDirectory())
-//			System.out.println(f.getName());
 	}
 
 }
